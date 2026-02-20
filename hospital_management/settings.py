7cbb5972f1a4ef1560434e9ce4e8ee177e9f6bc9
Gouse Velluri <gousevelluri444@gmail.com>
2026-02-20 23:37:35 +0530

    Update session settings for production deployment

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -128,3 +128,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
